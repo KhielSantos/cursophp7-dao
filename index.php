61ddfd65b26332054cfee1347055bba6eaf562ea
KhielSantos <eziquielsantos@gmail.com>
2019-04-03 11:30:57 -0300

    busca em lista

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Busca um Usuario
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root->loadById(3);
+//Busca uma lista de usuario
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+//Busca uma lista pelo login
+// $search = Usuario::search("roo");
+// echo json_encode($search);
 
+//Carrega usuario por login e senha
+$usuario = new Usuario();
+$usuario->login("root", "654321");
+
+echo $usuario;
 ?>
